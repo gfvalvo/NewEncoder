76dfcc8e4e7dde15bbbb3249b15b7dc936a6d796
gfvalvo <gfvalvo@gmail.com>
2020-12-20 08:12:57 -0500

    Updated example

--- examples/CustomEncoder/CustomEncoder.ino
+++ examples/CustomEncoder/CustomEncoder.ino
@@ -2,13 +2,13 @@
 #include "NewEncoder.h"
 
 // Demonstrate creation of custom Encoder using inheritance.
-// This encoder has asymmetric value changes for up / down and ignores the min / max value limits.
+// This encoder has asymmetric value changes for up / down and has no min / max value limits.
 
 class CustomEncoder : public NewEncoder {
   public:
     CustomEncoder() : NewEncoder() {}
-    CustomEncoder(uint8_t aPin, uint8_t bPin, int16_t minValue, int16_t maxValue, int16_t initalValue, uint8_t type = FULL_PULSE) :
-      NewEncoder(aPin, bPin, minValue, maxValue, initalValue, type) {}
+    CustomEncoder(uint8_t aPin, uint8_t bPin, int16_t initalValue, uint8_t type = FULL_PULSE) :
+      NewEncoder(aPin, bPin, initalValue - 1, initalValue + 1, initalValue, type) {}
     virtual ~CustomEncoder() {}
 
   protected:
@@ -25,8 +25,8 @@ void CustomEncoder::updateValue(uint8_t updatedState) {
 
 // Pins 2 and 3 should work for many processors, including Uno. See README for meaning of constructor arguments.
 // Use FULL_PULSE for encoders that produce one complete quadrature pulse per detnet, such as: https://www.adafruit.com/product/377
-// Use HALF_PULSE for endoders that produce one complete quadrature pulse for every two detents, such as: https://www.mouser.com/ProductDetail/alps/ec11e15244g1/?qs=YMSFtX0bdJDiV4LBO61anw==&countrycode=US&currencycode=USD
-CustomEncoder encoder(2, 3, -20, 20, 0, FULL_PULSE);
+// Use HALF_PULSE for endoders that produce one complete quadrature pulse for every two detents, such as: https://www.adafruit.com/product/377
+CustomEncoder encoder(2, 3, 0, FULL_PULSE);
 
 int16_t prevEncoderValue;
 
